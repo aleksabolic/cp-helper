b95ff8daff624d0a9f08e0184f19187ea11089c0
bolicaleksa <aleksa.bolic@gmail.com>
2025-03-31 22:44:10 +0200

    setup react + tailwind

---
+++ web/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div class="p-6 bg-white rounded-lg shadow-lg">
+        <h1 class="text-3xl font-bold text-blue-600">Tailwind is working!</h1>
+        <p class="mt-4 text-gray-700">This is a sample text.</p>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
